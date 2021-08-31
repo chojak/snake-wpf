0414ac4a5e3b773b4cad1050d605a9e263d1732a
Łukasz <chojecki2000@gmail.com>
2021-09-01 01:50:14 +0200

    It works (probably)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,59 +19,84 @@ namespace snake
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public class Position : Window
+    {
+        public double x, y;
+        public Position()
+        {
+
+        }
+        Position(double a, double b)
+        {
+            this.x = a;
+            this.y = b;
+        }
+        public Position GetFoodPosition(double x, double y, List<Rectangle> snakebody)
+        {
+            Random rand = new Random();
+            double newX = (double)rand.Next(30) * 25;
+            double newY = (double)rand.Next(30) * 25;
+
+            foreach (Rectangle piece in snakebody)
+            {
+                if ((double)piece.GetValue(TopProperty) == newY && (double)piece.GetValue(LeftProperty) == newX)
+                    return GetFoodPosition(x, y, snakebody);
+            }
+            return new Position(newX, newY);
+        }
+    }
     public partial class MainWindow : Window
     {
         DispatcherTimer timer;
         Random rand;
-        Rectangle[] snakebody;
+        List<Rectangle> snakebody;
         int score;
         enum direction
         {
             Up,
             Down,
             Left,
-            Right
+            Right,
+            Nowhere
         }
-        direction snakeDirection;
+        direction snakeDirection = direction.Nowhere;
+        bool foodEaten = false;
+        double lastX, lastY;
         public MainWindow()
         {
             InitializeComponent();
 
             timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
             timer.Tick += timer_Tick;
+            snakebody = new List<Rectangle>();
 
             rand = new Random();
-            snakebody = new Rectangle[900];
         }
         private void start_Click(object sender, RoutedEventArgs e)
         {
+            score = 0;
+            snakeDirection = direction.Nowhere;
             int rand1, rand2;
             do
             {
-                rand1 = rand.Next(0, 30);
-                rand2 = rand.Next(0, 30);
+                rand1 = rand.Next(30);
+                rand2 = rand.Next(30);
             } while (rand1 == 15 && rand2 == 15);
- 
+
             foreach (var x in snakebody)
             {
                 Snake.Children.Remove(x);
             }
+            snakebody.Clear();
 
-            for (int i = 0; i < snakebody.Length; i++)
-            {
-                snakebody[i] = new Rectangle();
-             // snakebody[i].Stroke = Brushes.White;
-                snakebody[i].Fill = Brushes.White;
-             // snakebody[i].HorizontalAlignment = HorizontalAlignment.Left;
-             // snakebody[i].VerticalAlignment = VerticalAlignment.Center;
-                snakebody[i].Width = 25;
-                snakebody[i].Height = 25;
-            }
+            snakebody.Add(new Rectangle());
+            snakebody[0].Fill = Brushes.GreenYellow;
+            snakebody[0].Width = 25;
+            snakebody[0].Height = 25;
             snakebody[0].SetValue(TopProperty, (double)375);
             snakebody[0].SetValue(LeftProperty, (double)375);
             Snake.Children.Add(snakebody[0]);
-            
 
             score = 0;
             food.SetValue(TopProperty, (double)rand1 * 25);
@@ -79,31 +104,69 @@ namespace snake
             food.Visibility = Visibility.Visible;
             timer.Start();
         }
-
         private void timer_Tick(object sender, EventArgs e)
         {
+            scoreTextBox.Text = "Score: " + score;
+            for (int i = snakebody.Count() - 1; i > 0; i--)
+            {
+                snakebody[i].SetValue(TopProperty, snakebody[i - 1].GetValue(TopProperty));
+                snakebody[i].SetValue(LeftProperty, snakebody[i - 1].GetValue(LeftProperty));
+            }
+
             moveSnake(snakebody[0], snakeDirection);
 
-            double topProp = (double)snakebody[0].GetValue(TopProperty);
-            double leftProp = (double)snakebody[0].GetValue(LeftProperty);
-
-            double food
-
-            if (leftProp < 0 || leftProp > 725 || topProp < 0 || topProp > 725)
+            if(foodEaten)
             {
-                MessageBox.Show("Game Over");
-                //this.Close();
-            }    
+                growSnake(lastX, lastY, snakebody);
+                foodEaten = false;
+                string temp = "";
 
-            if(leftProp == food.g)
+             /* foreach(var x in snakebody)
+                {
+                    temp += "x: " + x.GetValue(LeftProperty) + "y: " + x.GetValue(TopProperty) + "\n";
+                }
+                MessageBox.Show(temp);
+             */
+            }
 
+            double headY = (double)snakebody[0].GetValue(TopProperty);
+            double headX = (double)snakebody[0].GetValue(LeftProperty);
 
-            
+            double foodTopProp = (double)food.GetValue(TopProperty);
+            double foodLeftProp = (double)food.GetValue(LeftProperty);
+
+            if (headX < 0 || headX > 725 || headY < 0 || headY > 725)
+            {
+                MessageBox.Show("Game Over, your score: " + score);
+                timer.Stop();
+            }
+            if (headX == foodLeftProp && headY == foodTopProp)
+            {
+                score++;
+
+                foodEaten = true;
+                lastX = (double)snakebody[snakebody.Count() - 1].GetValue(LeftProperty);
+                lastY = (double)snakebody[snakebody.Count() - 1].GetValue(TopProperty);
+
+                Position position = new Position().GetFoodPosition(rand.Next(30) * 25, rand.Next(30) * 25, snakebody);
+                Canvas.SetLeft(food, position.x);
+                Canvas.SetTop(food, position.y);
+            }
+            foreach(var piece in snakebody)
+            {
+                if (headX == (double)piece.GetValue(LeftProperty)   && 
+                    headY == (double)piece.GetValue(TopProperty)    && 
+                    snakebody.Count() > 1                           &&
+                    piece != snakebody[0])
+                {
+                    MessageBox.Show("Game Over, your score: " + score);
+                    timer.Stop();
+                }
+            }
         }
-
         private void OnKeyDownHandler(object sender, KeyEventArgs e)
         {
-            if(e.Key == Key.Up && snakeDirection != direction.Down)
+            if (e.Key == Key.Up && snakeDirection != direction.Down)
             {
                 snakeDirection = direction.Up;
             }
@@ -122,29 +185,43 @@ namespace snake
         }
         private void moveSnake(Rectangle head, direction dir)
         {
-            double topProp = (double)head.GetValue(TopProperty);
-            double leftProp = (double)head.GetValue(LeftProperty);
-            switch(dir)
+            double headY = (double)head.GetValue(TopProperty);
+            double headX = (double)head.GetValue(LeftProperty);
+            switch (dir)
             {
                 case direction.Up:
-                    head.SetValue(TopProperty, topProp - (double)25);
+                    head.SetValue(TopProperty, headY - (double)25);
                     break;
 
                 case direction.Down:
-                    head.SetValue(TopProperty, topProp + (double)25);
+                    head.SetValue(TopProperty, headY + (double)25);
                     break;
 
                 case direction.Left:
-                    head.SetValue(LeftProperty, leftProp - (double)25);
+                    head.SetValue(LeftProperty, headX - (double)25);
                     break;
 
                 case direction.Right:
-                    head.SetValue(LeftProperty, leftProp + (double)25);
+                    head.SetValue(LeftProperty, headX + (double)25);
                     break;
-   
+
                 default:
                     break;
-            }    
+            }
+        }
+        private void growSnake(double x, double y, List<Rectangle> snakebody)
+        {
+            Rectangle temp = new Rectangle();
+
+            temp.Fill = Brushes.YellowGreen;
+            temp.Width = 25;
+            temp.Height = 25;
+            temp.SetValue(TopProperty, lastY);
+            temp.SetValue(LeftProperty, lastX);
+
+            snakebody.Add(temp);
+
+            Snake.Children.Add(snakebody[snakebody.Count() - 1]);
         }
     }
 }
